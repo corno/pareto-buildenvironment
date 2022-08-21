9ab61501bc506c126250e3b2f54cb408cde7acd1
corno <corno.astn.a@schraverus.com>
2022-08-21 10:50:11 +0200

    .

--- data/projectTemplate/pareto/scripts/copyTemplate.sh
+++ data/projectTemplate/pareto/scripts/copyTemplate.sh
@@ -3,7 +3,7 @@ dir=`realpath $(dirname "$0")`
 cp -R ./node_modules/pareto-buildenvironment/data/projectTemplate/. .. \
 
 #npm messes with .gitignore, that's why I need to handle it separately
-cp ./node_modules/pareto-ts-validator-lib/data/gitignore ../.gitignore \
+cp ./node_modules/pareto-buildenvironment/data/gitignore ../.gitignore \
 
 
 if [ -d "../pub/" ]
@@ -27,5 +27,5 @@ do
 
 done
 
-#update this package because the copied package.json contains an old version of pareto-ts-validator-lib (by design, the template is not updated every time the package is published)
+#update this package because the copied package.json contains an old version of pareto-buildenvironment (by design, the template is not updated every time the package is published)
 "$dir/updatePackage.sh" pareto
