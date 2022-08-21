d98cc72b87cf6908d20d1b070f6677792261427b
corno <corno.astn.a@schraverus.com>
2022-08-21 20:24:36 +0200

    .

--- copyTemplate.sh
+++ copyTemplate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 rootDirOfProject = $1
+
+echo "copying to $rootDirOfThisProject"
 dirOfThisScript=`realpath $(dirname "$0")`
 cp -R $dirOfThisScript/data/projectTemplate/. $rootDirOfProject/pareto \
 
