c545a5e8f54012a239e9757beba928000d6baed4
corno <corno.astn.a@schraverus.com>
2022-09-01 17:04:21 +0200

    .

--- copyTemplate.sh
+++ copyTemplate.sh
@@ -14,6 +14,12 @@ cp $dirOfThisScript/data/gitignore $rootDirOfProject/.gitignore \
 root="`cd "$rootDirOfProject";pwd`" # the resolved path to the root dir of the project
 rootName=`basename $root`
 
+if [ -d "$rootDirOfProject/dev/" ]
+then
+    cp $dirOfThisScript/data/tsconfig.json "$rootDirOfProject/dev/"
+    cp $dirOfThisScript/data/_globals.ts "$rootDirOfProject/dev/src/"
+fi
+
 if [[ $rootName == res-* || $rootName == pareto-core-* ]]
 then
     echo "$rootName; not copying typescript files to pub"
@@ -24,13 +30,15 @@ else
     cp $dirOfThisScript/data/_globals.ts $rootDirOfProject/pub/src/
 fi
 
-parts=("dev" "test")
-for part in "${parts[@]}"
-do
-    if [ -d "$rootDirOfProject/$part/" ]
-    then
-        cp $dirOfThisScript/data/tsconfig.json "$rootDirOfProject/$part/"
-        cp $dirOfThisScript/data/_globals.ts "$rootDirOfProject/$part/src/"
-    fi
-
-done
+if [[ $rootName == api-* ]]
+then
+    echo "$rootName; not copying test files"
+else
+    cp $dirOfThisScript/data/tsconfig.json "$rootDirOfProject/test/"
+    cp $dirOfThisScript/data/_globals.ts "$rootDirOfProject/test/src/"
+    cp $dirOfThisScript/data/test.ts "$rootDirOfProject/test/src/bin/"
+    
+    cd "$rootDirOfProject/test/"
+    npm install res-pareto-filesystem@latest
+    npm install res-pareto-diff@latest
+fi
