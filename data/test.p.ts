9b05b982bc032b713b3214daa6be22a225df3830
corno <corno.astn.a@schraverus.com>
2022-09-17 03:16:43 +0200

    .

--- data/test.p.ts
+++ data/test.p.ts
@@ -4,10 +4,7 @@ import * as pe from "pareto-core-exe"
 
 import * as test from "lib-pareto-test"
 
-import * as diff from "res-pareto-diff"
-import * as fs from "res-pareto-filesystem"
-
-import { createGetTestset } from "../imp"
+import { createGetTestset } from "../implementation"
 import { dependencies } from "../dependencies/dependencies"
 
 
@@ -17,15 +14,7 @@ pe.runProgram(
             getTestSet: createGetTestset(
                 dependencies
             ),
-            diff: {
-                diffData: diff.diffData,
-                stringsAreEqual: diff.stringsAreEqual,
-            },
-            fs: {
-                readFile: fs.readFile,
-                writeFile: fs.writeFile,
-                unlink: fs.unlink,
-            },
+            dependencies: test.dependencies,
         },
     )
 )
