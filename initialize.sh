0dc4f269a6eedaec4d78a63f330a1660db1ce620
corno <corno.astn.a@schraverus.com>
2022-11-24 15:21:55 +0100

    .

--- initialize.sh
+++ initialize.sh
@@ -25,16 +25,25 @@ if [ -d $devDir ]
 then
     cp $dirOfThisScript/data/tsconfig.json "$devDir"
     cp $dirOfThisScript/data/_globals.ts "$devDir/src/"
-    npm --prefix "$devDir" install pareto-core@latest
+    npm --prefix "$devDir" install pareto-core-exe@latest
+    npm --prefix "$devDir" install pareto-core-lib@latest
+    npm --prefix "$devDir" install pareto-core-raw@latest
+    npm --prefix "$devDir" install pareto-core-state@latest
 fi
 
 
 #pub
+if [[ $rootName == exe-* ]]
+then
+    npm --prefix "$pubDir" install pareto-core-exe@latest
+fi
 if [[ $rootName == lib-* || $rootName == exe-* ]]
 then
     cp $dirOfThisScript/data/tsconfig.json "$pubDir"
     cp $dirOfThisScript/data/_globals.ts "$pubDir/src/"
-    npm --prefix "$pubDir" install pareto-core@latest
+    npm --prefix "$pubDir" install pareto-core-lib@latest
+    npm --prefix "$pubDir" install pareto-core-raw@latest
+    npm --prefix "$pubDir" install pareto-core-state@latest
 fi
 
 if [[ $rootName == res-* ]]
@@ -49,6 +58,10 @@ then
     cp $dirOfThisScript/data/tsconfig.json "$testDir/"
     cp $dirOfThisScript/data/_globals.ts "$testDir/src/"
     cp $dirOfThisScript/data/test.generated.p.ts "$testDir/src/bin/"
-    npm --prefix "$testDir" install pareto-core@latest
+    npm --prefix "$testDir" install pareto-core-exe@latest
+    npm --prefix "$testDir" install pareto-core-lib@latest
+    npm --prefix "$testDir" install pareto-core-raw@latest
+    npm --prefix "$testDir" install pareto-core-state@latest
     npm --prefix "$testDir" install lib-pareto-test@latest
+    npm --prefix "$testDir" install lib-pareto-exe@latest
 fi
