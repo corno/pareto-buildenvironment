dde6f879019d2c8b811edb1b489450a3597721c4
corno <corno.astn.a@schraverus.com>
2023-01-29 07:18:15 +0100

    .

--- data/projectTemplate/build/scripts/setFingerprints.sh
+++ data/projectTemplate/build/scripts/setFingerprints.sh
@@ -27,17 +27,14 @@ then
 
     #get the version property from the package.json file
     #then trim the quotes before and after
-    version=$(npm pkg get version | cut -c2- | rev | cut -c2- |rev) 
 
     #the version property is mandatory for the 'npm pack' command, so I temporarily set it to '0.0.0'
-    npm pkg set version=0.0.0
     npm pkg delete content-fingerprint
 
     #create a package, but don't store it (--dry-run), let the summary output be json
     #create a shasum of that and then trim to the first 40 characters of that shasum (the rest is filename info, which in this case is: ' -')
     contentfingerprint=$(npm pack --dry-run --json | shasum | cut -c1-40)
 
-    npm pkg set version="$version" #restore version
     npm pkg set content-fingerprint="$contentfingerprint" #restore version
 
     popd
