3a622b9effa058204da14058bd7bf4479c0aa529
corno <corno.astn.a@schraverus.com>
2023-01-30 00:35:26 +0100

    .

--- data/projectTemplate/build/scripts/buildAndTest.sh
+++ data/projectTemplate/build/scripts/buildAndTest.sh
@@ -5,9 +5,6 @@ rootDir="$scriptDir/../.."
 
 "$scriptDir/prebuild.sh" && \
 
-"$scriptDir/checkPackageUpdates.sh" pub && \
-"$scriptDir/checkPackageUpdates.sh" test && \
-
 "$scriptDir/buildPubAndTestPackages.sh" && \
 
 "$scriptDir/test.sh"
