e5e055ac4aef3c49f31ac58f4e545d251eef737a
corno <corno.astn.a@schraverus.com>
2023-01-29 07:03:47 +0100

    .

--- data/projectTemplate/build/scripts/updateWorkspace2latestBuildEnvironment.sh
+++ data/projectTemplate/build/scripts/updateWorkspace2latestBuildEnvironment.sh
@@ -4,6 +4,5 @@ workspaceDir=$1
 
 for f in $workspaceDir/*; do
     cd "$f/build" && \
-    "./scripts/updatePackage.sh" "build" && \
-    "./node_modules/build-buildenvironment/copyTemplate.sh" "$f"
+    "./scripts/updatePackage.sh" "build"
 done
