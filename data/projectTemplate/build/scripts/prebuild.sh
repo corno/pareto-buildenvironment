8e76f0db73c1704ea392ca12eec3e4ce4b670267
corno <corno.astn.a@schraverus.com>
2023-01-29 10:24:08 +0100

    .

--- data/projectTemplate/build/scripts/prebuild.sh
+++ data/projectTemplate/build/scripts/prebuild.sh
@@ -3,9 +3,6 @@
 scriptDir=`realpath $(dirname "$0")`
 rootDir="$scriptDir/../.."
 
-root="`cd "$rootDir";pwd`" # the resolved path to the root dir of the project
-rootName=`basename $root`
-
 if [ -d "$rootDir/dev" ]
 then
     "$scriptDir/buildDevPackage.sh" && \
--- data/projectTemplate/build/scripts/test.sh
+++ data/projectTemplate/build/scripts/test.sh
@@ -3,6 +3,9 @@
 scriptDir=`realpath $(dirname "$0")`
 rootDir="$scriptDir/../.."
 
+root="`cd "$rootDir";pwd`" # the resolved path to the root dir of the project
+rootName=`basename $root`
+
 if [[ $rootName == glo-* || $rootName == pareto-core-types ]]
 then
     echo "$rootName; no testing for glossary or core-types"
