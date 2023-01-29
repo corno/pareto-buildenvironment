2224ad3b3ab3ca7edcc8bb801394ae765fdc3c3d
corno <corno.astn.a@schraverus.com>
2023-01-29 07:54:42 +0100

    .

--- data/projectTemplate/build/scripts/setVersion2LatestPublished.sh
+++ data/projectTemplate/build/scripts/setVersion2LatestPublished.sh
@@ -3,11 +3,13 @@
 scriptDir=`realpath $(dirname "$0")`
 rootDir="$scriptDir/../.."
 
+pushd "$rootDir/pub" > /dev/null && \
+
 name=$(npm pkg get name | cut -c2- | rev | cut -c2- |rev) && \
 
 remoteVersion=$(npm view $name@latest version) && \
 
-pushd "$rootDir/pub" > /dev/null && \
+echo ">>>>>>>>>$name>>>>>>>$remoteVersion" && \
 
 npm pkg set version="$remoteVersion" && \
 
