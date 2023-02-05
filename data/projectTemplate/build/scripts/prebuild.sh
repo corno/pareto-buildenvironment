1f1e90f40b73527fd397b16f8f6da1f9fa38ba02
corno <corno.astn.a@schraverus.com>
2023-02-05 14:20:40 +0100

    .

--- data/projectTemplate/build/scripts/prebuild.sh
+++ data/projectTemplate/build/scripts/prebuild.sh
@@ -7,14 +7,14 @@ if [ -d "$rootDir/dev" ]
 then
     "$scriptDir/buildDevPackage.sh" && \
     npx tsc -p "$rootDir/dev" && \
-    node "$rootDir/dev/dist/bin/generateCode.generated.js" ../..
+    node "$rootDir/dev/dist/bin/generateCode.generated.js" "$rootDir"
 fi && \
 
 rm -rf "$rootDir/tmp/templates" && \
 
 "$scriptDir/buildParetoPackage.sh" && \
 npx tsc -p "$rootDir/pareto" && \
-node "$rootDir/pareto/dist/bin/generateCode.generated.js" ../.. && \
+node "$rootDir/pareto/dist/bin/generateCode.generated.js" "$rootDir" && \
 
 "$scriptDir/checkPackageUpdates.sh" "pub" && \
 "$scriptDir/checkPackageUpdates.sh" "test"
