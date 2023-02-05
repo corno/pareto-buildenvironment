afd90812cc2de9c6d90d34ad756abaede047e984
corno <corno.astn.a@schraverus.com>
2023-02-05 14:29:42 +0100

    .

--- data/projectTemplate/build/scripts/updateDependenciesAndBuild.sh
+++ data/projectTemplate/build/scripts/updateDependenciesAndBuild.sh
@@ -6,4 +6,6 @@ rootDir="$scriptDir/../.."
 "$scriptDir/updatePackage.sh" pub && \
 "$scriptDir/updatePackage.sh" test && \
 
-"$scriptDir/buildAndTest.sh"
+"$scriptDir/buildPubAndTestPackages.sh" && \
+
+"$scriptDir/test.sh"
