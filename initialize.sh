0731876711c846962b8aab7b02ab43abb18c92bc
corno <corno.astn.a@schraverus.com>
2023-03-01 11:05:07 +0100

    .

--- initialize.sh
+++ initialize.sh
@@ -6,4 +6,4 @@ echo "run this script from the ./scripts directory"
 echo "copying to $rootDirOfProject"
 dirOfThisScript=`realpath $(dirname "$0")`
 rm -rf $rootDirOfProject/scripts/scripts/*
-cp -R $dirOfThisScript/data/projectTemplate/ $rootDirOfProject \
+cp -R $dirOfThisScript/data/projectTemplate/. $rootDirOfProject \
