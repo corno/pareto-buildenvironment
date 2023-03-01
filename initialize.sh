113d911acd2309868f420b41426e2bf10b9ec205
corno <corno.astn.a@schraverus.com>
2023-03-01 11:01:56 +0100

    .

--- initialize.sh
+++ initialize.sh
@@ -2,7 +2,8 @@
 paretoBuildEnvDir=$1
 rootDirOfProject="$1/.."
 
+echo "run this script from the ./scripts directory"
 echo "copying to $rootDirOfProject"
 dirOfThisScript=`realpath $(dirname "$0")`
 rm -rf $rootDirOfProject/scripts/scripts/*
-cp -R $dirOfThisScript/data/projectTemplate/. $rootDirOfProject \
+cp -R $dirOfThisScript/data/projectTemplate $rootDirOfProject \
