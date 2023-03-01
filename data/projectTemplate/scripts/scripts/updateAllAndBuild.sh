859106d609059e3b72e1d3d5878aef008adaf532
corno <corno.astn.a@schraverus.com>
2023-03-01 11:20:38 +0100

    .

--- data/projectTemplate/scripts/scripts/updateAllAndBuild.sh
+++ data/projectTemplate/scripts/scripts/updateAllAndBuild.sh
@@ -4,7 +4,7 @@ scriptDir=`realpath $(dirname "$0")`
 rootDir="$scriptDir/../.."
 
 #make sure latest buildenvironment is installed
-"$scriptDir/updateBuildEnvironment.sh" && \
+"$scriptDir/updateScripts.sh" && \
 
 #update packages and build
 "$scriptDir/updatePrebuildDependencies.sh" && \
