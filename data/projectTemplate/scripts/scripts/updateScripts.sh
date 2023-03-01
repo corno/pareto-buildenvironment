f982713c5879e396c69d8a32a6ac56744e664886
corno <corno.astn.a@schraverus.com>
2023-03-01 17:16:26 +0100

    .

--- data/projectTemplate/scripts/scripts/updateScripts.sh
+++ data/projectTemplate/scripts/scripts/updateScripts.sh
@@ -5,5 +5,4 @@ buildDir="$scriptsDir/.."
 
 node "$buildDir/node_modules/npm-updatedependencies2latest/dist/index.js" "$buildDir" && \
 npm update
-
-"$buildDir/node_modules/pareto-buildenvironment/initializeProject.sh" "$buildDir"
+"$buildDir/node_modules/pareto-buildenvironment/initializeProject.sh" "$buildDir/.."
