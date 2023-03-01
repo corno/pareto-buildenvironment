c0c1cc1199e9a91b450017936d3959d5155ba408
corno <corno.astn.a@schraverus.com>
2023-03-01 11:14:53 +0100

    .

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-paretoBuildEnvDir=$1
-rootDirOfProject="$1/.."
 
-echo "run this script from the ./scripts directory"
+if [ -z "$1" ]
+  then
+    echo "No path to root of project supplied"
+fi
+
+rootDirOfProject="$1"
+
 echo "copying to $rootDirOfProject"
 dirOfThisScript=`realpath $(dirname "$0")`
 rm -rf $rootDirOfProject/scripts/scripts/*
