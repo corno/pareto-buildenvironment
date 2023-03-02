547798846972736ccbb63fa8c843677a8afc06c3
corno <corno.astn.a@schraverus.com>
2023-03-02 09:28:30 +0100

    .

--- data/projectTemplate/scripts/scripts/buildFromScratch.sh
+++ data/projectTemplate/scripts/scripts/buildFromScratch.sh
@@ -4,10 +4,6 @@ scriptDir=`realpath $(dirname "$0")`
 rootDir=`realpath "$scriptDir/../.."`
 buildDir="$rootDir/scripts"
 
-#change to a directory that will not be deleted
-cd $rootDir && \
-pushd $rootDir && \
-
 "$scriptDir/clean.sh" && \
 npm install pareto-buildenvironment --prefix "$buildDir" && \
 "$buildDir/initialize.sh" && \
