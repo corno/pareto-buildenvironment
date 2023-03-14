9de1c8ff47b18ba95a8a38bb94d6a98cdfca580b
corno <corno.astn.a@schraverus.com>
2023-03-14 13:09:26 +0100

    .

--- data/projectTemplate/scripts/scripts/initializePareto.sh
+++ data/projectTemplate/scripts/scripts/initializePareto.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 scriptDir=`realpath $(dirname "$0")`
 
+rootDir=`realpath "$scriptDir/../.."`
+
 cp -RT $scriptDir/../node_modules/pareto-buildenvironment/data/pareto $rootDir/pareto
