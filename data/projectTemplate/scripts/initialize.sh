fb56275dd82f5cc44699ffd2ca21289177af6553
Corno Schraverus <corno@schraverus.com>
2024-09-01 19:48:19 +0200

    .

--- data/projectTemplate/scripts/initialize.sh
+++ data/projectTemplate/scripts/initialize.sh
@@ -3,4 +3,5 @@
 paretoBuildEnvDir=`realpath $(dirname "$0")`
 
 npm install && \
+node ./node_modules/npm-updatedependencies2latest/dist/index.js . dependencies && \
 "$paretoBuildEnvDir/node_modules/pareto-buildenvironment/initializeProject.sh" "$paretoBuildEnvDir/.."
