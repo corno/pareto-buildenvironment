9483954bd23264b758dc04a1e61492164d0636b2
Corno Schraverus <corno@schraverus.com>
2024-09-02 10:24:51 +0200

    .

--- data/projectTemplate/scripts/scripts/buildFromScratch.sh
+++ data/projectTemplate/scripts/scripts/buildFromScratch.sh
@@ -5,7 +5,6 @@ rootDir=`realpath "$scriptDir/../.."`
 buildDir="$rootDir/scripts"
 
 "$scriptDir/cleanExceptScripts.sh" && \
-npm install pareto-buildenvironment --prefix "$buildDir" && \
 "$buildDir/initialize.sh" && \
 #"$buildDir/node_modules/pareto-buildenvironment/initializeProject.sh $rootDir" && \
 #"$scriptDir/initalizePareto.sh" && \
